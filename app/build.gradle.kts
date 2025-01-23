791d2432c65c299cad0ed9220118ed34b9e51a8a
kirillVolkovich <kirillvolkovich1@gmail.com>
2025-01-23 19:00:28 +0300

    step 3

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("com.github.ben-manes.versions") version "0.52.0"
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
