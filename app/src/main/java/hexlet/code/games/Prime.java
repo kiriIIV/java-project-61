651ebca336d965da6a6a7bfff22841e139548d04
kirillVolkovich <kirillvolkovich1@gmail.com>
2025-01-25 17:12:07 +0300

    fix some files

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -33,7 +33,11 @@ public class Prime {
 
     public static String isPrime(int a) {
 
-        for (int i = 2; i < a / 2; i++) {
+        if (a == 1) {
+            return "no";
+        }
+
+        for (int i = 2; i <= a / 2; i++) {
             if (a % i == 0) {
                 return "no";
             }
